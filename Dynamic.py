917e160bad78d2875220689e5545df914760b834
WilliamMK07 <wmk208@exeter.ac.uk>
2026-02-03 13:27:22 +0000

    ADded dynamic of one constraint(time)

--- Dynamic.py
+++ Dynamic.py
@@ -1,3 +1,4 @@
+import time
 def dynamic(file):
     # This part of the function is responsible for reading the file.
     f = open(file,"r") # file is opened in read mode
@@ -14,58 +15,91 @@ def dynamic(file):
         activity[3] = activity[3].replace("\n",'') # removes the newline character
         activities.append(activity) # appends it to the activities array
     #print(activities)
-    time_table = []
-    for i in range(len(activities)):
-        if any( time_table_row [2] == activities[i][1] for time_table_row in time_table) :
-            for item in range(len(time_table)):
-                if activities[i][1] == time_table[item][2]:
-                    if activities[i][3] > time_table[item][3]:
-                        time_table[item] == activities[i]
-                        break
-        else:
-            time_table.append([activities[i][1]]+activities[i])
-    print(time_table)
     f.close() # closes the file
-    find(activities,budget,time,time_table) 
- 
-def find(activities,budget,time,time_table):
-    if any( time_table_row [0] == str(time) for time_table_row in time_table) :
-        print(time)
-        for i in time_table:
-            if i[2] == str(time) :
-                return i[0:]
-    else:
-        #print(time)
-        return find(activities,budget,time-1,time_table)
-
-def max(activities,time,time_table):
-    
-
-
-dynamic("Lists/input_10 (1).txt")
-
-
+    dp = (dp_table(activities,n,time) )
+    #print(dp)
+    return(dp_best(dp,activities,n,time,budget))
 
 #dp Table   attempt 1 
 #, how it works  creates a table  with enjoyment and time   and adds  activitys for best enjoyment  for every  number of activies  for every maxing time 
 #  for example  0 activities the best enjoyement will be 0  for each ax time ,  1 activity the best enjoyment will be tdecided best on the  available time  and so forth 
- def dp_table( activities,n,time):
-     #storing  maximum enjoyment and time 
-     dp = [[0 for  i in range (time+1)
-               for  k in range(n+1)] # creates a 2d table with 0s 
+def dp_table(activities,n,time):
+    #storing  maximum enjoyment and time 
+    dp = [[0 for  i in range (time+1)]for  k in range(n+1)] # creates a 2d table with 0s 
            
     for i  in range (1,n+1):  # decide best enjoyement deepending on time 
-           for t  in range(time+1)
+           for t  in range(time+1):
             activity_time = int(activities[i-1][1]) # gets time for current activity 
             activity_enjoyment =  int(activities[i-1][3]) # gets enjoyment for current  activity 
-            #
+
             if activity_time <= t: #  compares  activity time to  the  current time limit  and gives two options 
                 #   option one takes the activity and  adds the enjoyment 
-                dp[i][t] = max(dp[i-1][t],dp[i-1][t-activity_time] + activity enjoyment)  finds the highest 
+                dp[i][t] = max(dp[i-1][t],dp[i-1][t-activity_time] + activity_enjoyment)#  finds the highest 
+
+                #store which activity 
+                
             else:
                 dp[i][t] = dp[i-1][t] #  option 2 skips  the activity and go to the next one 
-    return dp 
-           
+    # for i in range(len(dp)-1,0,-1):
+    #     print(i,dp[i])
+    return dp
+
+def dp_best(dp,activities,n,time,budget):
+    activities_list = []
+    current = [dp[n][time],n,time]
+    #print(current)
+    #print(dp[current[1]][time], dp[current[1]-1][time])
+    activities_list = []
+    while current[0] != 0:
+        #print(current)
+        #print(dp[current[1]][current[2]], dp[current[1]-1][current[2]])
+        if dp[current[1]][current[2]] > dp[current[1]-1][current[2]]:
+            #print(activities[current[1]-1],"sdfghjk")
+            activities_list.append(activities[current[1]-1])
+            current = [dp[current[1]-1][current[2]-int(activities[current[1]-1][1])],current[1]-1,current[2]-int(activities[current[1]-1][1])]
+        else:
+            current[1] = current[1]-1
+        
+    #print(activities_list,"GHJK")
+    time_used = 0
+    cost = 0
+    enjoyment = 0
+    for i in range(len(activities_list)):
+        time_used += int(activities_list[i][1]) 
+        cost += int(activities_list[i][2])
+        enjoyment += int(activities_list[i][3])
+    return activities_list, enjoyment, cost, time_used, budget,time
+
+def final_output_d(file): # this is the function that creates the final output of the functions. 
+    
+    start = time.time() # starts the time
+    brute_force = dynamic(file) # runs the function and saves the value to a variable 
+    end = time.time() # ends the time
+    speed = end - start # calculates the time taken for the function to run
+
+    # space to add the dynamic approach:
+
+    start2 = time.time()
+    # add the dynamic function here
+    end2 = time.time()
+    speed2 = end2-start2
+
+
+    # formatting the output from the brute force algorithm, as it returns an array the indexes of the array can be accessed
+    # and they contain all of the necessary information for the final output
+
+    print("--- DYNAMIC PROGRAMMING ALGORITHM ---")
+    print("Selected Activities:")
+    for i in range(len(brute_force[0])):
+        print(f" - {brute_force[0][i][0]} ({brute_force[0][i][1]} hours, £{brute_force[0][i][2]}, enjoyment {brute_force[0][i][3]})")
+    print('')
+    print(f"Total Enjoyment: {brute_force[1]}")
+    print(f"Total Time Used: {brute_force[3]} hours")
+    print(f"Total Cost: £{brute_force[2]}\n")
+    print(f"Execution time: {speed:.4f} seconds")
+
+final_output_d("Lists/input_small.txt")
+    
            
            
      
