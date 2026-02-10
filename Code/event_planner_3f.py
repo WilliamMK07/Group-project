45bc048e5b89f01c563b0d7c9595d4c2edb588e1
alexlecornu <al1119@exeter.ac.uk>
2026-02-10 12:50:20 +0000

    made the 3f file work correctly

--- Code/event_planner_3f.py
+++ Code/event_planner_3f.py
@@ -1,5 +1,65 @@
 import time
 import sys
+from itertools import chain, combinations
+
+# this is the main function that calculates the best combination using the brute-force method
+def brute_combo(file):
+    # This part of the function is responsible for reading the file.
+    f = open(file,"r") # file is opened in read mode
+    n = int(f.readline()) # first line is read and this value is the number of activities
+    b_t = f.readline() # second line is read which is the budget and the time
+    b_t=b_t.split(" ",2) # the budget and time are split into an array
+    budget = int(b_t[1]) # The budget is in the array at index 1, currently stored as a string so switched to an int 
+    time = int(b_t[0]) # The time is in the array at index 0, currently stored as a string so switched to an int 
+
+    activities = [] # Creates an empty array for all of the activities
+    for i in range(n): # loops through the array and adds the activities to the activities array
+        activity = f.readline()
+        activity = activity.split(" ",4) # splits the line into an array
+        activity[3] = activity[3].replace("\n",'') # removes the newline character
+        activities.append(activity) # appends it to the activities array
+
+    f.close() # closes the file
+
+    options = powerset(activities) # uses the powerset function to get the powerset of the activities
+    # sets the inital conditions for the loop.
+    result = '' 
+    max_happiness = 0
+    max_budget = 0
+    max_time = 0
+    
+    # loop to iterate through the powerset and find the best possible combination
+    for i in range(len(options)):
+        # Sets the inital conditions inside the loop
+        h = False
+        t = False
+        b = False
+        total_h = 0
+        total_b = 0
+        total_t = 0
+        for j in range(len(options[i])): # this loop is used to add up the happiness, budget and time of each subset in the array
+            total_h += int(options[i][j][3])
+            total_b += int(options[i][j][2])
+            total_t += int(options[i][j][1])
+        if total_h > max_happiness: # evaulates if the happiness is greater than the previous happiness
+            h = True
+        if total_t <= time: # checks if the time is less than the time limit
+            t = True
+        if total_b <= budget:
+            b = True
+        if h and t: # checks if all 3 conditions are true then changes the max_happiness and updates the budget and time
+            result = options[i] # stores the subset of the activities as the result
+            max_happiness = total_h
+            max_budget = total_b
+            max_time = total_t
+    # return the best subset which is the result, the happiness,budget and time of this result. and the time and budget limits from the file
+    return result, max_happiness, max_budget, max_time, budget,time
+
+def powerset(array): # this is the function that is used to create the powerset of the array
+    s = list(array) # makes sure the array is a list
+    # returns the list of activities by making every subset combination of the list. It starts from 1 instead of 0
+    # as the value at index 0 will be the empty set. This would cause errors when indexing later. 
+    return list(chain.from_iterable(combinations(s, r) for r in range(1,len(s)+1)))
 
 
 def read_input(filename):
@@ -71,20 +131,29 @@ def dynamic_programming(filename):
     )
 
 #runs the code and times the execution
-def print_results(filename):
+def final_output(file): # this is the function that creates the final output of the functions. 
+    
+    start = time.time() # starts the time
+    brute_force = brute_combo(file) # runs the function and saves the value to a variable 
+    end = time.time() # ends the time
+    speed = end - start # calculates the time taken for the function to run
+
+    # space to add the dynamic approach:
+
     start_time = time.time()
-    activities, happiness, time_used, budget_used, max_time, max_budget = dynamic_programming(filename)
+    activities, happiness, time_used, budget_used, max_time, max_budget = dynamic_programming(file)
     end_time = time.time()
     execution_time = end_time - start_time
 
 
-    print("========================================")
-    print("EVENT PLANNER - DYNAMIC PROGRAMMING")
-    print("========================================\n")
+    # formatting the output from the brute force algorithm, as it returns an array the indexes of the array can be accessed
+    # and they contain all of the necessary information for the final output
+    print('======================================== \nEVENT PLANNER - RESULTS \n========================================\n')
+    print(f"Input File: {file}")
+    print(f"Available Time: {brute_force[5]}")
+    print(f"Available Budget: {brute_force[4]}\n")
 
-    print(f"Available Time: {max_time}")
-    print(f"Available Budget: £{max_budget}\n")
-
+    print("--- DYNAMIC PROGRAMMING ALGORITHM ---")
     print("Selected Activities:")
     for a in activities:
         print(f" - {a[0]} ({a[1]} hours, £{a[2]}, enjoyment {a[3]})")
@@ -95,7 +164,20 @@ def print_results(filename):
     print(f"Total Cost: £{budget_used}")
     
     print(f"\nExecution Time: {execution_time:.6f} seconds")
+    
 
-#runs the program
+    print("--- BRUTE FORCE ALGORITHM ---")
+    print("Selected Activities:")
+    for i in range(len(brute_force[0])):
+        print(f" - {brute_force[0][i][0]} ({brute_force[0][i][1]} hours, £{brute_force[0][i][2]}, enjoyment {brute_force[0][i][3]})")
+    print('')
+    print(f"Total Enjoyment: {brute_force[1]}")
+    print(f"Total Time Used: {brute_force[3]} hours")
+    print(f"Total Cost: £{brute_force[2]}\n")
+    print(f"Execution time: {speed:.4f} seconds")
+
+
+#/workspaces/Group-project/Lists/input_10 (1).txt
+
 if __name__ == "__main__":
-    final_output(sys.argv[1]) 
+    final_output(sys.argv[1])    
